88fd7a6121d852aa4800b7adb9b465c99b34ad68
Fidel Ernesto <fidehlg89@gmail.com>
2022-02-03 20:39:02 -0500

    Add scss package Update currencies component

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="dashboard-page">
+    <CurrencyConvert />
+  </div>
+</template>
+
+<script>
+import CurrencyConvert from "../currency";
+export default {
+  name: "Dashboard",
+  components: {
+    CurrencyConvert,
+  },
+};
+</script>
+
+<style scoped></style>
